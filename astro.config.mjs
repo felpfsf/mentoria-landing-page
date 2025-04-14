c1b7a7791d8f405144de00263b630278d7ea0de8
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-14 17:15:48 -0300

    🔧 add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
   output: "server",
+  adapter: vercel(),
 });
