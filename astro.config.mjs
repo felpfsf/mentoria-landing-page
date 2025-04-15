ef33fc8b43865b0ab9dde614dfea1785600d992e
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-15 16:30:19 -0300

    ✨ Enhance SEO and branding by adding meta tags, updating layout, and integrating sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,15 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  site: "https://mentoria.kamilasantos.dev", // todo: colocar o link do site
   output: "server",
   adapter: vercel(),
 });
